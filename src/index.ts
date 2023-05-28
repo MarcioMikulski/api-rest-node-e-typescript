e949ffeee979efe8698ba234c37325d8cfd49f61
Marcio Mikulski <mugemikulski@gmail.com>
2023-05-28 17:49:32 -0300

    feat: initial config

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { server } from './server/Server';
+
+
+server.listen(8080, () => console.log('App Rodando'));
---
+++ src/server/Server.ts
@@ -0,0 +1,13 @@
+import  express  from 'express';
+
+
+const server = express();
+
+
+server.get('/', (_, res) =>{
+    return res.send('Olá dev');
+});
+
+
+export { server} ;
+
